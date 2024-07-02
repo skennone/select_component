6c55b371f948775625185694f9f78fdc3b1da9e9
Menas Abdalla <m.abdalla@standardit.eu>
2024-07-02 15:43:57 +0200

    select

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+import { Select, SelectOption } from "./Select"
+
+const options = [
+  { label: "First", value: 1 },
+  { label: "Second", value: 2 },
+  { label: "Third", value: 3 },
+  { label: "Fourth", value: 4 },
+  { label: "Fifth", value: 5 },
+]
+
+function App() {
+  const [value, setValue] = useState<SelectOption | undefined>(options[0])
+  return (
+    <>
+      <Select options={options} value={value} onChange={o => setValue(o)} />
+    </>
+  )
+}
+
+export default App
